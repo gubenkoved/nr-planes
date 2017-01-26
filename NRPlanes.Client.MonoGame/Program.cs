3f9c830ef4302f7da2cb1c0a2c9684dc47396bc7
Gubenkov Eugene <gubenkoved@gmail.com>
2017-01-26 21:55:00 +0400

    show errors on start

--- NRPlanes.Client.MonoGame/Program.cs
+++ NRPlanes.Client.MonoGame/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace NRPlanes.Client.MonoGame
 {
@@ -14,8 +15,28 @@ namespace NRPlanes.Client.MonoGame
         [STAThread]
         static void Main()
         {
-            using (var game = new PlanesGame())
-                game.Run();
+            try
+            {
+                using (PlanesGame game = new PlanesGame())
+                {
+                    game.Run();
+                }
+            }
+            catch (Exception exc)
+            {
+                var fileStream = new FileStream("errors.txt", FileMode.Append);
+
+                var streamWriter = new StreamWriter(fileStream);
+
+                using (streamWriter)
+                {
+                    streamWriter.WriteLine(DateTime.Now);
+                    streamWriter.WriteLine(exc.GetType().ToString());
+                    streamWriter.WriteLine(exc.Message);
+                    streamWriter.WriteLine(exc.StackTrace);
+                    streamWriter.WriteLine(Environment.NewLine);
+                }
+            }
         }
     }
 #endif
